a5dd126103f95876b49eac24eb50b7b98d243766
alex <alexnbui@gmail.com>
2023-06-10 02:28:37 -0500

    Completed frontend in React. Still non-functional.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,44 @@
 import Navbar from "./Components/Navbar.jsx"
+import Form from "./Components/Form.jsx";
+import { ReactSketchCanvas } from 'react-sketch-canvas';
+import Results from "./Components/Results.jsx";
+import "./App.css"
 import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
+document.body.style = 'background: black;';
 
+const canvasStyle = {
+    border: "0.0625rem solid #ffffff",
+    borderRadius: "0.25rem",
+    marginBottom: "20px",
+    width: "100%",
+    aspectRatio: "1 / 1"
+}
+
+export default function App() {
     return (
         <>
             <Navbar />
+            <div className="container">
+                <div className="row row-cols-md-2 row-cols-sm-1">
+                    <div className="col-md-8" style={{ marginTop: "40px" }}>
+                        <div className="row">
+                            <ReactSketchCanvas
+                                canvasColor="#000000"
+                                style={canvasStyle}
+                                strokeWidth={10}
+                                strokeColor="white"
+                                marginBottom
+                            />
+                        </div>
+                        <div className="row">
+                            <Form />
+                        </div>
+                    </div>
+                    <Results />
+                </div>
+            </div >
         </>
     )
 }
 
-export default App
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -2,9 +2,9 @@ import gitlogo from "../assets/github-mark.png"
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand">
+        <nav className="navbar navbar-expand" style={{ backgroundColor: "#ffffff" }}>
             < div className="container" >
-                <a href="#" class="navbar-brand">Kanji Classifier</a>
+                <a href="#" className="navbar-brand">Kanji Classifier</a>
                 <a href="https://github.com/al3xbro/cnn-ocr-kanji"> <img src={gitlogo} width="30" /> </a>
             </div >
         </nav >
