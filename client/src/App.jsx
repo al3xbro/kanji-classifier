1251bf062dd24c2ef4b78a257ac0b2251005529e
Chloe <chloevision97@gmail.com>
2023-06-11 05:33:30 +0200

    Fix upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,23 @@
-import Navbar from "./Components/Navbar.jsx"
-import Canvas from "./Components/Canvas.jsx"
+import Navbar from "./Components/Navbar.jsx";
+import Canvas from "./Components/Canvas.jsx";
 import Results from "./Components/Results.jsx";
-import "./App.css"
-import 'bootstrap/dist/css/bootstrap.css';
-
-document.body.style = 'background: black;';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+document.body.style = "background: black;";
+const queryClient = new QueryClient();
 export default function App() {
-    return (
-        <>
-            <Navbar />
-            <div className="container">
-                <div className="row row-cols-md-2 row-cols-sm-1">
-                    <div className="col-md-8" style={{ marginTop: "40px" }}>
-                        <Canvas />
-                    </div>
-                    <Results />
-                </div>
-            </div >
-        </>
-    )
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Navbar />
+      <div className="container">
+        <div className="row row-cols-md-2 row-cols-sm-1">
+          <div className="col-md-8" style={{ marginTop: "40px" }}>
+            <Canvas />
+          </div>
+          <Results />
+        </div>
+      </div>
+    </QueryClientProvider>
+  );
 }
-
