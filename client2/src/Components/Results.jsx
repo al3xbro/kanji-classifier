209ae153108fda92e35f64abae4dda0babd9dbbb
Alex Bui <112739079+al3xbro@users.noreply.github.com>
2023-07-02 23:46:24 -0500

    Revert "Next"

--- client2/src/Components/Results.jsx
+++
@@ -1,8 +0,0 @@
-export default function Results() {
-    return (
-        <div className="col-md-4" style={{ marginTop: 40 }}>
-            <div id="best-guess">日</div>
-            <div id="best-certainty">98%</div>
-        </div>
-    )
-}
