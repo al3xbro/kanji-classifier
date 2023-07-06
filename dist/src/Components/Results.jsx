8f50e136ee3103427b65dbab5bb9790b1ca85364
alex <alexnbui@gmail.com>
2023-07-06 14:44:21 -0500

    Added hover effect to guesses

--- dist/src/Components/Results.jsx
+++ dist/src/Components/Results.jsx
@@ -5,8 +5,8 @@ export default function Results() {
         <div className="col-md-4" style={{ marginTop: 40 }}>
             {cert && predict && cert.map((item, index) => {
                 return <>
-                    <a id="best-guess" href={`https://jisho.org/search/%23kanji%20${predict[index]}`} target="_blank">{predict[index]}</a>
-                    <div id="best-certainty">{Math.trunc(item * 100) + "%"}</div></>
+                    <a id="guesses" href={`https://jisho.org/search/%23kanji%20${predict[index]}`} target="_blank">{predict[index]}</a>
+                    <div id="certainty">{Math.trunc(item * 100) + "%"}</div></>
             })}
         </div>
     )
