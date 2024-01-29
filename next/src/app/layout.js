66645f0966b6a2dc5cf579ec03a46f4783d544e9
Alex Bui <alexnbui@gmail.com>
2024-01-29 03:11:31 -0800

    Add analytics

--- next/src/app/layout.js
+++ next/src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import "bootstrap/dist/css/bootstrap.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
     title: 'Kanji Classifier',
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body style={{ backgroundColor: "black" }}>{children}</body>
+            <Analytics />
         </html>
     )
 }
